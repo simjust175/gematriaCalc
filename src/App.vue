25a4e40e3924038afc53495aed72ce36e0337431
simjust175 <bunim175@gmail.com>
2024-12-19 01:33:26 +0100

    file added

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <v-app class="bg-alef-beis" theme="light" id="app">
+      <v-app-bar scroll-behavior="elevate inverted" class="bg-grey-lighten-2 mr-10" height="45">
+        <v-toolbar-title>
+          <v-img src="../public/images/gemariaLogo.png" max-width="40"></v-img>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        
+      </v-app-bar>
+      <v-main class="d-flex align-center justify-center">
+        <gim-form />
+      </v-main>
+    </v-app>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Audiowide&family=Berkshire+Swash&family=Chakra+Petch:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Dancing+Script:wght@400;500;700&family=Happy+Monkey&family=Noto+Rashi+Hebrew:wght@100..900&display=swap');
+
+.rashi {
+  font-family: "Noto Rashi Hebrew", serif;
+}
+
+#app {
+  background: url("../public/images/alef_beis.png") no-repeat center center fixed !important;
+  background-size: cover;
+}
+</style>
---
+++ src/components/GimForm.vue
@@ -0,0 +1,101 @@
+<template>
+  <v-card class="pa-10 rashi bg-grey-lighten-2" min-width="600">
+    <v-locale-provider rtl>
+      <v-text-field
+        label="Enter gematria"
+        variant="outlined"
+        class="text-h2 py-10 rashi"
+        v-model="gimatria"
+        :loading="loading"
+        :rules="gimatriaRules"
+        clearable
+      ></v-text-field>
+    <v-row>
+      <v-col cols="6">
+        <v-card variant="outlined" cols="5" min-height="150">
+          <v-card-title class="pr-1 text-h6">גימטריה:</v-card-title>
+          <div class="pa-8 pt-1 pl-1 d-flex align-center justify-center">
+            <h2 class="text-center">{{ gimatriaCalc("full") }}</h2>
+          </div>
+        </v-card>
+      </v-col>
+      <v-col cols="6">
+        <v-card variant="outlined" cols="5" min-height="150">
+          <v-card-title class="pr-1 text-h6">מספר קטן:</v-card-title>
+          <div class="pa-8 pt-1 pl-1 d-flex align-center justify-center">
+            <h2 class="text-center">{{ gimatriaCalc("short") }}</h2>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+    
+</v-locale-provider>
+  </v-card>
+</template>
+
+<script setup>
+import { reactive, ref } from "vue";
+
+const gimatria = ref("");
+
+const gimatriaRules = ref([
+  (value) => {
+    if (!value) return true;
+    for (let char of value) {
+      if (char !== " " && !gimatriaValues[char]) {
+        return "Oy vey! Only alef-bais will work.";
+      }
+    }
+    return true;
+  },
+]);
+
+const gimatriaValues = reactive({
+  א: { full: 1, short: 1 },
+  ב: { full: 2, short: 2 },
+  ג: { full: 3, short: 3 },
+  ד: { full: 4, short: 4 },
+  ה: { full: 5, short: 5 },
+  ו: { full: 6, short: 6 },
+  ז: { full: 7, short: 7 },
+  ח: { full: 8, short: 8 },
+  ט: { full: 9, short: 9 },
+  י: { full: 10, short: 1 },
+  כ: { full: 20, short: 2 },
+  ך: { full: 20, short: 2 },
+  ל: { full: 30, short: 3 },
+  מ: { full: 40, short: 4 },
+  ם: { full: 40, short: 4 },
+  נ: { full: 50, short: 5 },
+  ן: { full: 50, short: 5 },
+  ס: { full: 60, short: 6 },
+  ע: { full: 70, short: 7 },
+  פ: { full: 80, short: 8 },
+  ף: { full: 80, short: 8 },
+  צ: { full: 90, short: 9 },
+  ץ: { full: 90, short: 9 },
+  ק: { full: 100, short: 1 },
+  ר: { full: 200, short: 2 },
+  ש: { full: 300, short: 3 },
+  ת: { full: 400, short: 4 },
+});
+
+const loading = ref(false);
+const setLoading = () => {
+  loading.value = true;
+  setTimeout(() => {
+    loading.value = false;
+  }, 1500);
+};
+
+const gimatriaCalc = (stat) => {
+  const gim = gimatria.value.trim();
+  if (!gim) return null;
+  setLoading()
+  const gimatriaArray = gim.split("").filter((char) => gimatriaValues[char]);
+  const array = gimatriaArray.map((char) => gimatriaValues[char][stat]);
+  return array.reduce((total, value) => total + value, 0);
+};
+</script>
+
+<style></style>
