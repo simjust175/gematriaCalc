bd3003019e07bfdd7f053d4b501d9a0ae07c26cc
simjust175 <bunim175@gmail.com>
2024-12-19 01:54:43 +0100

    added footer

---
+++ src/components/FooterComp.vue
@@ -0,0 +1,10 @@
+<template>
+    <v-footer theme="dark" padless>
+      <v-col class="text-center" cols="12">
+        <div class="d-flex justify-center align-center">
+          <div>{{ new Date().getFullYear() }} — </div>
+          <v-img src="../../../public/logo/dreamware.png" max-width="50" class="ml-8"></v-img>
+        </div>
+      </v-col>
+    </v-footer>
+  </template>
